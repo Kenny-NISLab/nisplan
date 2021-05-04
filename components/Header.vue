f2e9eb2aac6f4f30bcea474346549c1363d20c7d
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-04 19:34:33 +0900

    #3 Add Header view

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="flex justify-around p-4">
+    <BackPage />
+    <h1>
+      <img
+        src="@/assets/images/NISPLAN.png"
+        alt="NISPLAN"
+        class="w-32 max-w-full"
+      />
+    </h1>
+    <Submit />
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
