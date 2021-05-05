1683dfa93bc8657dfa93cb2af9a95f870341ec56
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-05 12:02:05 +0900

    #6 Update defalt layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <Nuxt />
+  <div class="bg-gray-300">
+    <Header />
+    <main class="bg-white w-full mx-auto rounded-2xl">
+      <Nuxt />
+    </main>
+  </div>
 </template>
 
 <script>
