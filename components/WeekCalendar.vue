5ac1b9bde89833ab47399897976a8d1da4be635e
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-06 19:40:35 +0900

    Update moment

---
+++ components/WeekCalendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3></h3>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  computed: {
+    Calendar() {
+      return this.$store.state.calendar
+    },
+  },
+}
+</script>
