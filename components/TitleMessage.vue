6087f1687686eea50c9976d3a41b01de34f52e31
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-10 19:10:35 +0900

    Develop TitleMessage

--- components/TitleMessage.vue
+++ components/TitleMessage.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="text-center py-5 font-bold">
-    <h2>誰を？</h2>
+    <h2>{{ message }}</h2>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+      default: 'No Message',
+    },
+  },
+}
+</script>
