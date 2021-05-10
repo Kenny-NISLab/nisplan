f8389daf48af8d79606348551126009f3bf49321
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-10 16:16:55 +0900

    Develop register view

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>hello</h1>
+  <div class="bg-white w-full mx-auto rounded-2xl z-0">
+    <SelectStudents />
+  </div>
 </template>
+<script>
+export default {
+  layout: 'lowerPage',
+}
+</script>
