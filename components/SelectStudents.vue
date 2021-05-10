49162a459f32b066297ed24d4f712c0073873caa
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-10 16:14:19 +0900

    Add Component

---
+++ components/SelectStudents.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="flex justify-around flex-wrap w-11/12 mx-auto pt-5">
+    <div v-for="(student, index) in Students" :key="index">
+      <div class="mr-1 mb-5">
+        <img
+          :src="student.avatar"
+          :alt="student.name + 'のプロフィール画像'"
+          class="block rounded-lg w-10 mx-auto"
+        />
+        <span>{{ student.j_last_name }}{{ student.j_first_name }}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    Students() {
+      return this.$store.state.students
+    },
+  },
+}
+</script>
---
+++ components/TitleMessage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-center py-5 font-bold">
+    <h2>誰を？</h2>
+  </div>
+</template>
