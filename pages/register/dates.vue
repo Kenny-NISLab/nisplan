7afddfc1eab6253a57c3fc712d7c3a3de0702c6f
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-19 14:25:07 +0900

    Modify test

--- pages/register/dates.vue
+++ pages/register/dates.vue
@@ -4,7 +4,7 @@
       <CommonTitleMessage message="When will you come?" />
     </div>
     <div
-      class="bg-white w-full h-screen test test fixed overflow-y-scroll pb-2 mx-auto rounded-2xl"
+      class="bg-white w-full h-screen test fixed overflow-y-scroll pb-2 mx-auto rounded-2xl"
     >
       <DateStudentIcon />
       <DateListDates :reserved="ReservedSchedule" />
