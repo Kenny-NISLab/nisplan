32fb3b9daceca77855cbc982d889ca6e6778aeff
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-26 11:01:35 +0900

    Update console

--- store/index.js
+++ store/index.js
@@ -58,6 +58,7 @@ export const mutations = {
     state.weather = weather
   },
   setOffset(state, { offset, index }) {
+    console.log('hello')
     state.offset[index] = offset
   },
   setActiveDate(state, active) {
